3c86e03f0ddcd8ff8d0857c96eccddf7bd363a3f
unknown <babuldeb101@gmail.com>
2021-09-27 14:29:00 +0000

    wind direction

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+let searchInp = document.querySelector(".weather__search");
+let city = document.querySelector(".weather__city");
+let day = document.querySelector(".weather__day");
+let humidity = document.querySelector(".weather__indicator--humidity>.value");
+let wind = document.querySelector(".weather__indicator--wind>.value");
+let pressure = document.querySelector(".weather__indicator--pressure>.value");
+let image = document.querySelector(".weather__image");
+let temperature = document.querySelector(".weather__temperature>.value");
+
 let weatherApiKey = "a5aaed97d516398c3d6e80326f9de7d0";
 let rootUrl =
   "https://api.openweathermap.org/data/2.5/weather?units=metric&appid=" +
@@ -6,6 +15,39 @@ let rootUrl =
 let getWeatherByCity = async (city) => {
   let endpoint = rootUrl + "&q=" + city;
   let response = await fetch(endpoint);
-  console.log(response.json());
+  let weather = await response.json();
+  return weather;
 };
-getWeatherByCity("london");
+
+searchInp.addEventListener("keydown", async (e) => {
+  if (e.keyCode === 13) {
+    let weather = await getWeatherByCity(searchInp.value);
+    updateCurrentWeather(weather);
+  }
+});
+
+let updateCurrentWeather = (data) => {
+  console.log(data);
+  city.textContent = data.name + ", " + data.sys.country;
+  day.textContent = dayOfWeek();
+  humidity.textContent = data.main.humidity;
+  pressure.textContent = data.main.pressure;
+  let windDirection;
+  let deg = data.wind.deg;
+  if (deg > 45 && deg <= 135) {
+    windDirection = "East";
+  } else if (deg > 135 && deg <= 225) {
+    windDirection = "South";
+  } else if (deg > 225 && deg <= 315) {
+    windDirection = "West";
+  } else {
+    windDirection = "North";
+  }
+  wind.textContent = windDirection + ", " + data.wind.speed;
+  temperature.textContent =
+    data.main.temp > 0 ? "+" + data.main.temp : data.main.temp;
+};
+
+let dayOfWeek = () => {
+  return new Date().toLocaleDateString("en-EN", { weekday: "long" });
+};
